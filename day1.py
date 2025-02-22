451db74e2f13f426a3c98adc674206cf82ec5518
Vivek Kumar Koundinya Atchutanna <75157810+atchutannavivekkumar@users.noreply.github.com>
2025-02-22 10:33:35 -0500

    Add files via upload

--- day1.py
+++ day1.py
@@ -1,5 +1,15 @@
+#exercise 1 
 patientName = input("what is the name ")
 patientAge = input("what is your age ")
  
-print(patientName)
-print(patientAge)
+#print(patientName)
+#print(patientAge)
+
+#excercise 2
+
+firstNumber = int(input("Enter the first number: "))
+secondNumber = float(input("enter the seconf number: "))
+
+sum = firstNumber+secondNumber
+
+print(sum)
