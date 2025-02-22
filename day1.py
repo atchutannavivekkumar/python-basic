7fb7ea647662ed75735c9d4fa19c9429fba72421
Vivek Kumar Koundinya Atchutanna <75157810+atchutannavivekkumar@users.noreply.github.com>
2025-02-22 10:48:56 -0500

    Add files via upload

--- day1.py
+++ day1.py
@@ -1,15 +1,26 @@
 #exercise 1 
-patientName = input("what is the name ")
-patientAge = input("what is your age ")
+#patientName = input("what is the name ")
+#patientAge = input("what is your age ")
  
 #print(patientName)
 #print(patientAge)
 
 #excercise 2
 
-firstNumber = int(input("Enter the first number: "))
-secondNumber = float(input("enter the seconf number: "))
+#firstNumber = int(input("Enter the first number: "))
+#secondNumber = float(input("enter the seconf number: "))
 
-sum = firstNumber+secondNumber
+#sum = firstNumber+secondNumber
 
-print(sum)
+#print(sum)
+
+#excercise 3
+
+PersonWeight = int(input("Enter your Weight: "))
+unitsWeight = str(input("If the weight is in kg enter K and if in Lbs enter L: "))
+
+if unitsWeight =="l":
+    PersonWeightlbs = PersonWeight/(2.2)
+    print(PersonWeightlbs)
+else:
+    print(PersonWeight)
