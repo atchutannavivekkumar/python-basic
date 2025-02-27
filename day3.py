7022d7acaf797af36efce277478e0b9424603097
Vivek Kumar Koundinya Atchutanna <75157810+atchutannavivekkumar@users.noreply.github.com>
2025-02-27 18:45:23 -0500

    Add files via upload

--- day3.py
+++ day3.py
@@ -1,11 +1,25 @@
 #ifs 
-num1 = int(input("enter no 1: "))
-num2 = int(input("enter no 2: "))
+#num1 = int(input("enter no 1: "))
+#num2 = int(input("enter no 2: "))
 
-if num1 == num2:
-    print(" The both numbers({},{})are equal".format(num1,num2))
-elif num1 < num2:
-    print("{} is less than {}".format(num1,num2))
-else:
-    print("{} is less than {}".format(num2,num1))
-3
+# num1 == num2:
+ #   print(" The both numbers({},{})are equal".format(num1,num2))
+#elif num1 < num2:
+ #   print("{} is less than {}".format(num1,num2))
+#else:
+#    print("{} is less than {}".format(num2,num1))
+
+#for letter in 'geeksforgeeks':
+ #   if letter == 'v' or letter == 's':
+  #      break
+
+#print('Current Letter :', letter)
+
+#while
+
+name = str(input("Enter your name: "))
+i=0
+while  i<3:
+    i = i+1
+    print("This is my name {}".format(name))
+    
