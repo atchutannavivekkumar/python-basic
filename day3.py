5a073cb822ff27f449dd00153fb4576145d26097
Vivek Kumar Koundinya Atchutanna <75157810+atchutannavivekkumar@users.noreply.github.com>
2025-02-26 23:32:27 -0500

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,10 @@
+#ifs 
+num1 = int(input("enter no 1: "))
+num2 = int(input("enter no 2: "))
+
+if num1 == num2:
+    print("The both numbers are equal")
+elif num1 < num2:
+    print("num1 is less than num2")
+else:
+    print("num2 is less than Num1")
