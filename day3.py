7cedbe029f263a46b9943a6da98a3412cf5ed32c
Vivek Kumar Koundinya Atchutanna <75157810+atchutannavivekkumar@users.noreply.github.com>
2025-02-26 23:34:31 -0500

    Add files via upload

--- day3.py
+++ day3.py
@@ -3,8 +3,9 @@ num1 = int(input("enter no 1: "))
 num2 = int(input("enter no 2: "))
 
 if num1 == num2:
-    print("The both numbers are equal")
+    print(" The both numbers({},{})are equal".format(num1,num2))
 elif num1 < num2:
-    print("num1 is less than num2")
+    print("{} is less than {}".format(num1,num2))
 else:
-    print("num2 is less than Num1")
+    print("{} is less than {}".format(num2,num1))
+3
