e51710ad1bc95874dfbd08cb5907230652638679
Vivek Kumar Koundinya Atchutanna <75157810+atchutannavivekkumar@users.noreply.github.com>
2025-02-27 19:59:56 -0500

    Add files via upload

--- day4.py
+++ day4.py
@@ -1,10 +1,13 @@
 #question 1 
+#Q1: Write a Python program that prints the numbers from 1 to 10 using a for loop.
 
 #for i in range(1,11):
 
 #    print(i)
 
 #question 2
+#Q2: Write a Python program that prints the even numbers from 2 to 20 using a while loop.
+
 #i = 0
 
 #while i<21:
@@ -15,10 +18,29 @@
      #   print("not even")
 
 #question 3
+#Q3: Write a Python program that takes a number as input and prints its multiplication table up to 10 using a for loop.
+#num1 = int(input("enter number for multiplication: "))
+#num2 = int(input("Enter the number you want to multiply untill: "))
 
-num1 = int(input("enter number for multiplication: "))
+
+#for i in range(1,num2+1):
+ #   mul = num1 * i
+  #  print("{} X {} = {}".format(num1,i,mul))
+
+#question 4
+
+#Q4: Write a Python program that calculates the sum of all numbers from 1 to n, where n is given as input, using a while loop.
+
+num1 = int(input("Enter the no untill you want sum: "))
+
+j=0
+i = 1
+
+while i <= num1:
+    j = j+i  # Add current number to sum
+    i = i+1 
+
+print("The sum of 1 to {} is {}".format(num1,j))
 
 
-for i in range(1,11):
-    mul = num1 * i
-    print("{} X {} = {}".format(num1,i,mul))
+    
