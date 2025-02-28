cdc8f22494ba077f1b744ec893c90a90c629e21d
Vivek Kumar Koundinya Atchutanna <75157810+atchutannavivekkumar@users.noreply.github.com>
2025-02-27 19:13:41 -0500

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,24 @@
+#question 1 
+
+#for i in range(1,11):
+
+#    print(i)
+
+#question 2
+#i = 0
+
+#while i<21:
+ #   i = i+1
+  #  if i % 2 ==0:
+   #     print(i)
+    #else:
+     #   print("not even")
+
+#question 3
+
+num1 = int(input("enter number for multiplication: "))
+
+
+for i in range(1,11):
+    mul = num1 * i
+    print("{} X {} = {}".format(num1,i,mul))
